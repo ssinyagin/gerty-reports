163e7f58efd5f611e6812f3d85386cdc903496ce
Stanislav Sinyagin <ssinyagin@k-open.com>
2011-03-09 13:23:36 +0100

    choice between table and graph output

--- frontend/source/class/gertyreports/r/HDSL2_SHDSL_LINE_MIB_line.js
+++ frontend/source/class/gertyreports/r/HDSL2_SHDSL_LINE_MIB_line.js
@@ -65,15 +65,30 @@ qx.Class.define
              new qx.data.controller.List(daysModel, daysList);
              controlsContainer.add(daysList);
              
-             controlsContainer.add(new qx.ui.basic.Label("days"));
+             controlsContainer.add(new qx.ui.basic.Label("days."));
 
+             var spacer = new qx.ui.basic.Atom();
+             spacer.setWidth(20);
+             controlsContainer.add(spacer);
+             controlsContainer.add(new qx.ui.basic.Label("Output as:"));
+             
+             var outputTypeGroup =
+                 new qx.ui.form.RadioButtonGroup(new qx.ui.layout.HBox(3));
+             var outputTypeButton1 = new qx.ui.form.RadioButton("graph");
+             var outputTypeButton2 = new qx.ui.form.RadioButton("table");
+             outputTypeGroup.add(outputTypeButton1);
+             outputTypeGroup.add(outputTypeButton2);
+             outputTypeGroup.setSelection([outputTypeButton1]);
+             controlsContainer.add(outputTypeGroup);
+             
+             
              var plotContainer =
                  new qx.ui.container.Composite(new qx.ui.layout.Grow());
              rowsContainer.add(plotContainer, {flex: 1});
 
              // Event handler for selection changes
 
-             var drawGraph  = function() {
+             var displayData  = function() {
                  plotContainer.removeAll();
                  statusBar.setStatus("Loading...");
                  
@@ -101,9 +116,32 @@ qx.Class.define
                                  series[i][0] =
                                      new Date(series[i][0] * 1000);
                              }
-                             
-                             var plot = new qxdygraphs.Plot(series,opts);
-                             plotContainer.add(plot);
+
+                             if( outputTypeButton1.getValue() )
+                             {
+                                 var plot = new qxdygraphs.Plot(series,opts);
+                                 plotContainer.add(plot);
+                             }
+                             else
+                             {
+                                 var tableModel =
+                                     new qx.ui.table.model.Simple();
+                                 tableModel.setColumns(opts.labels);
+                                 tableModel.setData(series);
+
+                                 var table = new qx.ui.table.Table(tableModel);
+                                 var tcm = table.getTableColumnModel();
+                                 var timeRenderer =
+                                     new qx.ui.table.cellrenderer.Date();
+                                 var timeFormatter =
+                                     new qx.util.format.DateFormat(
+                                         "YYYY-MM-dd HH:mm");
+                                 timeRenderer.setDateFormat(timeFormatter);
+                                 tcm.setDataCellRenderer(0, timeRenderer);
+                                 table.setColumnWidth(0, 150);
+                                 table.setStatusBarVisible(false);
+                                 plotContainer.add(table);
+                             }
                              statusBar.setStatus(
                                  "Retrieved " + result[0] + " data points");
                          }
@@ -121,11 +159,12 @@ qx.Class.define
              };
              
              // redraw the graph on selection change
-             dateFrom.addListener("changeValue", drawGraph);
-             daysList.addListener("changeSelection", drawGraph);
-
+             dateFrom.addListener("changeValue", displayData);
+             daysList.addListener("changeSelection", displayData);
+             outputTypeGroup.addListener("changeSelection", displayData);
+             
              // plot the data when the window opens for the first time
-             drawGraph();
+             displayData();
          }
      }
  });
